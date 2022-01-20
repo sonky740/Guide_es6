774eafeff76ea46917c06250d94b7a07160e58af
손기연 <sonky740@minwise.co.kr>
2022-01-20 11:10:53 +0900

    ly-nav-content update

--- src/resources/js/layout/ly_nav.js
+++ src/resources/js/layout/ly_nav.js
@@ -5,30 +5,21 @@ const lyNav = () => {
   function show() {
     trigger.classList.add('on');
     target.classList.remove('hidden');
-    target.classList.add('shown');
     target.classList.add('on');
-
-    target.addEventListener('animationend', function () {
-      if (target.classList.contains('on')) {
-        target.classList.remove('on');
-      }
-    });
   }
 
   function hide() {
     trigger.classList.remove('on');
+    target.classList.remove('on');
     target.classList.add('off');
 
     target.addEventListener('animationend', function () {
-      if (target.classList.contains('off')) {
-        target.classList.remove('off');
-        target.classList.remove('shown');
-      }
+      target.classList.remove('off');
     });
   }
 
   trigger.addEventListener('click', function () {
-    if (target.classList.contains('shown')) {
+    if (target.classList.contains('on')) {
       hide();
     } else {
       show();
