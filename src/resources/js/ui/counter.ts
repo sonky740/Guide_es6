3e7b2410e299151a0d1b0e92a5558faef1ac15ea
손기연 <sonky740@ex-em.com>
2022-10-11 14:36:24 +0900

    counter easeOutCirc 적용

--- src/resources/js/ui/counter.ts
+++ src/resources/js/ui/counter.ts
@@ -8,7 +8,7 @@ const EVENT_KEY = `${NAME}`;
 class Counter extends BaseComponent {
   private _counter = 0;
   private _initNumber = 0;
-  private _duration = 0;
+  private _duration = 2000;
   private _comma = false;
   private _startTime = 0;
 
@@ -28,22 +28,35 @@ class Counter extends BaseComponent {
     this._step();
   }
 
+  _easingFn(x: number) {
+    // https://easings.net/
+    return Math.sqrt(1 - Math.pow(x - 1, 2)); // easeOutCirc
+  }
+
   _step() {
     const step = (currentTime: number) => {
       if (this._startTime === 0) {
         this._startTime = currentTime;
       }
-      const progress = Math.min((currentTime - this._startTime) / this._duration, 1);
+      const progress = this._easingFn(
+        Math.min((currentTime - this._startTime) / this._duration, 1)
+      );
       if (this._comma) {
-        this._element.innerHTML = numberComma(Math.floor(progress * (this._counter - this._initNumber) + this._initNumber));
+        this._element.innerHTML = numberComma(
+          Math.floor(
+            progress * (this._counter - this._initNumber) + this._initNumber
+          )
+        );
       } else {
-        this._element.innerHTML = Math.floor(progress * (this._counter - this._initNumber) + this._initNumber).toString();
+        this._element.innerHTML = Math.floor(
+          progress * (this._counter - this._initNumber) + this._initNumber
+        ).toString();
       }
       if (progress < 1) {
         window.requestAnimationFrame(step);
       } else {
         EventHandler.trigger(this._element, `${EVENT_KEY}.end`, {
-          target: this._element
+          target: this._element,
         });
       }
     };
